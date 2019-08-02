2caf2b01d08555a901786d4ad07cfbbf7e669054
Botmasher <languagejosh@hotmail.com>
2019-08-02 12:57:06 -1000

    Store input list and calculate current index

--- main.py
+++ main.py
@@ -1,23 +1,34 @@
+import copy
+
 class visiter:
-	def __init__(self, gen_iter):
-		self._iter = gen_iter()
+	def __init__(self, l):
+		self.l = l
+		self.iter = iter(l)
 		self.previous = None
-		self.end = len(self._iter) - 1
+		self.end = len(l) - 1
 		self.current_index = 0
-		self.current = list(self._iter)[self.current_index]
-		self.next = list(self._iter)[min(self.current_index + 1, self.end)]
+		self.current = l[self.get_index()]
+		self.next = l[self.get_index(increment=1)]
 
-	def get_next(self):
-		next_index = min(self.current_index + 1, self.end)
-		next_value = list(self._iter)[next_index]
-		return next_value
-
-	def __next__(self):
-		self.previous = self.current
-		self.current = next(self._iter)
-		self.current_index += 1
-		self.next = self.get_next()
+	def __call__(self):
 		return self
 
+	def __iter__(self):
+		return self
+
+	def __next__(self):
+		self.previous = self.l[self.get_index(-1)] if self.current_index > 0 else None
+		self.current = next(self.iter)
+		self.next = self.get_next() if self.current_index < self.end else None
+		self.current_index += 1
+		return self
+
+	def get_index(self, increment=0):
+		return min(self.current_index + increment, max(self.end, 0))
+
+	def get_next(self):
+		return self.l[self.get_index(1)]
+
 g = visiter([1, 2, 3, 4])
-[print(f"previous: {g.previous} -- current: {g.current} -- next: {g.next}") for n in g]
+for n in g:
+	print(f"previous: {g.previous} -- current: {g.current} -- next: {g.next}")
